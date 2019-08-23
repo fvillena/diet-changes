09858bd83b52057a02f87abb17e3fbef3cac9dde
fvillena <villenafabian@gmail.com>
2019-08-23 10:08:23 -0400

    now you just must use one dimensionality reduction method

--- analysis.py
+++ analysis.py
@@ -1,19 +1,24 @@
 from dietData.data.foodData import UN_food
 from dietData.data.dimReduction import Reduction
+import sklearn.manifold
 
-food_data = UN_food(
-    raw_dir=r'data/raw/FoodBalanceSheets_E_All_Data_(Normalized).csv',
-    country_dir=r'data/raw/country-group.xls'
-)
-food_data.clean_data()
-food_data.write_data(
-    save_dir=r'data/processed/dataset.csv'
-)
+# food_data = UN_food(
+#     raw_dir=r'data/raw/FoodBalanceSheets_E_All_Data_(Normalized).csv',
+#     country_dir=r'data/raw/country-group.xls'
+# )
+# food_data.clean_data()
+# food_data.write_data(
+#     save_dir=r'data/processed/dataset.csv'
+# )
 
 dim_reduction = Reduction(
     processed_data_dir=r'data/processed/dataset.csv',
 )
 
 dim_reduction.fit_transform(
-    reduced_data_dir=r'data/processed/'
+    reduced_data_dir=r'data/processed/dataset_2d_tsne.csv',
+    method = sklearn.manifold.TSNE(
+        verbose = 2,
+        random_state = 11
+    )
 )
--- dietData/data/dimReduction.py
+++ dietData/data/dimReduction.py
@@ -1,30 +1,25 @@
 import numpy as np
 import pandas as pd
 import sklearn.decomposition
-import sklearn.manifold
 
 class Reduction():
     def __init__(
         self,
         processed_data_dir=r'../data/processed/dataset.csv',
-        methods=[
-            ('pca', sklearn.decomposition.PCA(2)),
-            ('tsne', sklearn.manifold.TSNE(verbose = 2, random_state = 11))
-        ]
     ):
 
         self.processed_data = pd.read_csv(processed_data_dir)
-        self.methods = methods
 
     def fit_transform(
         self,
-        reduced_data_dir=r'../data/processed/'
+        method=sklearn.decomposition.PCA(2),
+        reduced_data_dir=r'../data/processed/dataset_2d_pca.csv'
     ):
-        for name,method in self.methods:
-            data_2d = method.fit_transform(self.processed_data[self.processed_data.columns[2:]])
-            data_2d = pd.concat([self.processed_data[['Area','Continent','Year Code']], pd.DataFrame(data_2d)], axis=1)
-            data_2d.columns = ['Area','Continent','Year Code', 'x', 'y']
-            data_2d.to_csv(
-                reduced_data_dir + 'dataset_2d_' + name + '.csv',
-                index=False
-            )
+        
+        data_2d = method.fit_transform(self.processed_data[self.processed_data.columns[2:]])
+        data_2d = pd.concat([self.processed_data[['Area','Continent','Year Code']], pd.DataFrame(data_2d)], axis=1)
+        data_2d.columns = ['Area','Continent','Year Code', 'x', 'y']
+        data_2d.to_csv(
+            reduced_data_dir,
+            index=False
+        )
