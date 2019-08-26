8e6937e06ac1ac12fdfde41ab0ecb3d1a145ca8e
fvillena <villenafabian@gmail.com>
2019-08-26 19:14:20 -0400

    adding arguments

--- analysis.py
+++ analysis.py
@@ -16,35 +16,35 @@ scatter_plot_dir = os.path.join(par_dir, r'reports/figures/scatter_')
 dim_reduction_method = ("pca" ,sklearn.decomposition.PCA(2))
 data_cut_columns = .05
 data_cut_rows = .05
-zero_as_na = True
+zero_as_na = False
 scaler = ("minmax",sklearn.preprocessing.MinMaxScaler())
+continent = None
 
-param_vals = (dim_reduction_method[0], scaler[0], str(data_cut_columns), str(data_cut_rows), str(zero_as_na))
+param_vals = (dim_reduction_method[0], scaler[0],str(continent), str(data_cut_columns), str(data_cut_rows), str(zero_as_na))
 
 processed_dir = os.path.join("data","processed")
 
 if not os.path.exists(os.path.join(par_dir,processed_dir)):
     os.makedirs(os.path.join(par_dir,processed_dir))
 
-processed_scaled_save_path = os.path.join(par_dir, processed_dir,"dataset_scaler-%s_cut-%s-%s_zanan-%s.csv" % param_vals[1:])
-processed_unscaled_save_path = os.path.join(par_dir, processed_dir, "dataset_cut-%s-%s_zanan-%s.csv" % param_vals[2:])
-dataset_2d_save_path = os.path.join(par_dir, processed_dir, "dataset_2d_reduction-%s_scaler-%s_cut-%s-%s_zanan-%s.csv" % param_vals)
-scatter_plot_dir = os.path.join(par_dir, "reports","figures","scatter_reduction-%s_scaler-%s_cut-%s-%s_zanan-%s" % param_vals)
-line_plot_dir = os.path.join(par_dir, "reports","figures","line_reduction-%s_scaler-%s_cut-%s-%s_zanan-%s" % param_vals)
-preprocessing_report_path = os.path.join(par_dir, "reports", "preprocessing-report_reduction-%s_scaler-%s_cut-%s-%s_zanan-%s.json" % param_vals)
-reduction_report_path = os.path.join(par_dir, "reports", "reduction-report_reduction-%s_scaler-%s_cut-%s-%s_zanan-%s.json" % param_vals)
+processed_scaled_save_path = os.path.join(par_dir, processed_dir,"dataset_scaler-%s_continent-%s_cut-%s-%s_zanan-%s.csv" % param_vals[1:])
+processed_unscaled_save_path = os.path.join(par_dir, processed_dir, "dataset_continent-%s_cut-%s-%s_zanan-%s.csv" % param_vals[2:])
+dataset_2d_save_path = os.path.join(par_dir, processed_dir, "dataset_2d_reduction-%s_scaler-%s_continent-%s_cut-%s-%s_zanan-%s.csv" % param_vals)
+scatter_plot_dir = os.path.join(par_dir, "reports","figures","scatter_reduction-%s_scaler-%s_continent-%s_cut-%s-%s_zanan-%s" % param_vals)
+line_plot_dir = os.path.join(par_dir, "reports","figures","line_reduction-%s_scaler-%s_continent-%s_cut-%s-%s_zanan-%s" % param_vals)
+preprocessing_report_path = os.path.join(par_dir, "reports", "preprocessing-report_reduction-%s_scaler-%s_continent-%s_cut-%s-%s_zanan-%s.json" % param_vals)
+reduction_report_path = os.path.join(par_dir, "reports", "reduction-report_reduction-%s_scaler-%s_continent-%s_cut-%s-%s_zanan-%s.json" % param_vals)
 
-if not os.path.exists(processed_scaled_save_path):
-    raw_data_dir = os.path.join(par_dir, "data", "raw", "FoodBalanceSheets_E_All_Data_(Normalized).csv")
-    country_dir = os.path.join(par_dir, "data", "raw", "country-group.xls")
-    food_data = UN_food(
-        raw_dir=raw_data_dir,
-        country_dir=country_dir)
-    food_data.clean_data(data_ratio_cut_columns=data_cut_columns)
-    food_data.make_report(report_path=preprocessing_report_path)
-    food_data.write_data(save_dir=processed_scaled_save_path, save_dir_unscaled=processed_unscaled_save_path)
-else:
-    food_data = Reduction(processed_data_dir=processed_scaled_save_path)
+raw_data_dir = os.path.join(par_dir, "data", "raw", "FoodBalanceSheets_E_All_Data_(Normalized).csv")
+country_dir = os.path.join(par_dir, "data", "raw", "country-group.xls")
+food_data = UN_food(
+    raw_dir=raw_data_dir,
+    country_dir=country_dir,
+    continent = continent)
+food_data.clean_data(data_ratio_cut_columns=data_cut_columns)
+food_data.make_report(report_path=preprocessing_report_path)
+food_data.write_data(save_dir=processed_scaled_save_path, save_dir_unscaled=processed_unscaled_save_path)
+
 
 
 dim_reduction = Reduction(processed_data_dir=processed_scaled_save_path)
@@ -73,6 +73,7 @@ scatter_plots = Scatter(
 
 
 scatter_plots.plot(
+    voronoi = True,
     file_dir_prefix = scatter_plot_dir,
     years=list(range(1961,2014)),
     extension = '.png'
