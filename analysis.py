cf54b410fa52318ccf3a934c734f1c77bc522830
fvillena <villenafabian@gmail.com>
2019-08-27 10:23:20 -0400

    neighborhood analysis

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@ from dietData.data.dimReduction import Reduction
 from dietData.visualization.plots import Scatter
 from dietData.visualization.plots import Line
 from dietData.analysis.movement import Movement
+from dietData.analysis.neighbors import Fitting
 import sklearn.decomposition
 import sklearn.preprocessing
 import os
@@ -35,54 +36,58 @@ scatter_plot_dir = os.path.join(par_dir, "reports","figures","scatter_reduction-
 line_plot_dir = os.path.join(par_dir, "reports","figures","line_reduction-%s_scaler-%s_continent-%s_cut-%s-%s_zanan-%s" % param_vals)
 preprocessing_report_path = os.path.join(par_dir, "reports", "preprocessing-report_reduction-%s_scaler-%s_continent-%s_cut-%s-%s_zanan-%s.json" % param_vals)
 reduction_report_path = os.path.join(par_dir, "reports", "reduction-report_reduction-%s_scaler-%s_continent-%s_cut-%s-%s_zanan-%s.json" % param_vals)
-movement_report_path = os.path.join(par_dir, "reports", "movement-report_reduction-%s_scaler-%s_continent-%s_cut-%s-%s_zanan-%s.json" % param_vals)
+movement_report_path = os.path.join(par_dir, "reports", "movement-report_reduction-%s_scaler-%s_continent-%s_cut-%s-%s_zanan-%s.csv" % param_vals)
+fitting_report_path = os.path.join(par_dir, "reports", "fitting-report_reduction-%s_scaler-%s_continent-%s_cut-%s-%s_zanan-%s.csv" % param_vals)
 
 
 
-# raw_data_dir = os.path.join(par_dir, "data", "raw", "FoodBalanceSheets_E_All_Data_(Normalized).csv")
-# country_dir = os.path.join(par_dir, "data", "raw", "country-group.xls")
-# food_data = UN_food(
-#     raw_dir=raw_data_dir,
-#     country_dir=country_dir,
-#     continent = continent)
-# food_data.clean_data(data_ratio_cut_columns=data_cut_columns)
-# food_data.make_report(report_path=preprocessing_report_path)
-# food_data.write_data(save_dir=processed_scaled_save_path, save_dir_unscaled=processed_unscaled_save_path)
+raw_data_dir = os.path.join(par_dir, "data", "raw", "FoodBalanceSheets_E_All_Data_(Normalized).csv")
+country_dir = os.path.join(par_dir, "data", "raw", "country-group.xls")
+food_data = UN_food(
+    raw_dir=raw_data_dir,
+    country_dir=country_dir,
+    continent = continent)
+food_data.clean_data(data_ratio_cut_columns=data_cut_columns)
+food_data.make_report(report_path=preprocessing_report_path)
+food_data.write_data(save_dir=processed_scaled_save_path, save_dir_unscaled=processed_unscaled_save_path)
 
 
 
-# dim_reduction = Reduction(processed_data_dir=processed_scaled_save_path)
+dim_reduction = Reduction(processed_data_dir=processed_scaled_save_path)
 
-# dim_reduction.fit_transform(
-#     reduced_data_dir=dataset_2d_save_path,
-#     method = dim_reduction_method[1]
-# )
+dim_reduction.fit_transform(
+    reduced_data_dir=dataset_2d_save_path,
+    method = dim_reduction_method[1]
+)
 
-# dim_reduction.make_report(report_path=reduction_report_path)
+dim_reduction.make_report(report_path=reduction_report_path)
 
 movement_analyzer = Movement(dataset_2d_save_path)
 movement_analyzer.calculate_save(movement_report_path)
 
-# line_plots = Line(
-#     not_normalized_dataset = processed_unscaled_save_path
-# )
+fitting_analyzer = Fitting(dataset_2d_save_path)
+fitting_analyzer.analyze_fitting(fitting_report_path)
+
+line_plots = Line(
+    not_normalized_dataset = processed_unscaled_save_path
+)
 
 
-# line_plots.plot(
-#     file_dir_prefix = line_plot_dir,
-#     countries=['Chile','United States of America'],
-#     extension = '.png'
-# )
+line_plots.plot(
+    file_dir_prefix = line_plot_dir,
+    countries=['Chile','United States of America'],
+    extension = '.png'
+)
 
-# scatter_plots = Scatter(
-#     bidimensional_dataset = dataset_2d_save_path
-# )
+scatter_plots = Scatter(
+    bidimensional_dataset = dataset_2d_save_path
+)
 
 
-# scatter_plots.plot(
-#     voronoi = True,
-#     file_dir_prefix = scatter_plot_dir,
-#     years=list(range(1961,2014)),
-#     extension = '.png'
-# )
+scatter_plots.plot(
+    voronoi = True,
+    file_dir_prefix = scatter_plot_dir,
+    years=list(range(1961,2014)),
+    extension = '.png'
+)
 
