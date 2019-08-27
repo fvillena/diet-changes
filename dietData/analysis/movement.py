56cd80a2edd6e39d27d3b417c2a01bd2c6966d49
fvillena <villenafabian@gmail.com>
2019-08-26 22:52:14 -0400

    movement analysis

---
+++ dietData/analysis/movement.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import json
+def euclideanDistance(a,b):
+    return np.linalg.norm(a-b)
+def distance(v1,v2,metric):
+    v1 = np.array(v1)
+    v2 = np.array(v2)
+    return metric(v1,v2)
+class Movement:
+    def __init__(self, bidimensional_dataset):
+        self.bidimensional_dataset = pd.read_csv(bidimensional_dataset)
+        self.countries = list(set(self.bidimensional_dataset.Area.values))
+    def calculate_save(self,destination_path):
+        movements = []
+        for country in self.countries:
+            distances = []
+            positions = []
+            for _,row in self.bidimensional_dataset[self.bidimensional_dataset.Area == country].iterrows():
+                positions.append((row['x'],row['y']))
+            for i in range(1,len(positions)):
+                d = distance(positions[i-1], positions[i], euclideanDistance)
+                distances.append(d)
+            movement = sum(distances)
+            movements.append((country,movement))
+        movements = sorted(movements, key=lambda x: x[1], reverse=True)
+        with open(destination_path, 'w', encoding='utf-8') as f:
+                    json.dump(movements, f, ensure_ascii=False, indent=4)
